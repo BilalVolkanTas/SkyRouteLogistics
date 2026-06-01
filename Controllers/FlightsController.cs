9b192100fa54ff3341a124f8077d82e6bb06e589
BilalVolkanTaş <148703222+BilalVolkanTas@users.noreply.github.com>
2026-06-01 10:27:33 +0300

    Remove overposting protection comments in FlightsController
    Removed comments about overposting protection from Create and Edit methods.

--- Controllers/FlightsController.cs
+++ Controllers/FlightsController.cs
@@ -49,8 +49,6 @@ namespace SkyRouteLogistics.Controllers
         }
 
         // POST: Flights/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,FlightNumber,Departure,Destination")] Flight flight)
@@ -81,8 +79,6 @@ namespace SkyRouteLogistics.Controllers
         }
 
         // POST: Flights/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,FlightNumber,Departure,Destination")] Flight flight)
